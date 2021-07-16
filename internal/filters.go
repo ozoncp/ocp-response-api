a4e22f34f6ab1535e591d80ebd1ea6be57459b31
gevorg kesyan <gkesyan@luxoft.com>
2021-07-16 16:24:15 +0300

    task-utils

--- internal/filters.go
+++ internal/filters.go
@@ -22,6 +22,17 @@ func FilterString(slice []string, f func(string) bool) []string {
 	return filtered
 }
 
+func FilterBytes(slice []byte, f func(byte) bool) []byte {
+	var filtered []byte
+	for _, item := range slice {
+		if f(item) {
+			filtered = append(filtered, item)
+		}
+	}
+
+	return filtered
+}
+
 func FilterFloat32(slice []float32, f func(float32) bool) []float32 {
 	var filtered []float32
 	for _, item := range slice {
@@ -35,6 +46,17 @@ func FilterFloat32(slice []float32, f func(float32) bool) []float32 {
 
 func FilterFloat64(slice []float64, f func(float64) bool) []float64 {
 	var filtered []float64
+	for _, item := range slice {
+		if f(item) {
+			filtered = append(filtered, item)
+		}
+	}
+
+	return filtered
+}
+
+func Filter(slice []interface{}, f func(interface{}) bool) []interface{} {
+	var filtered []interface{}
 	for _, item := range slice {
 		if f(item) {
 			filtered = append(filtered, item)
